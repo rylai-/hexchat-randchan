8568012f92279e0cc6a57bc0feefa1aa91d4179e
Rylai <gando.depth@gmail.com>
2013-11-03 14:14:28 -0500

    randchan: Lots of silly things.

--- randchan.py
+++ randchan.py
@@ -1,14 +1,14 @@
 import string
 import random
+import xchat
 
 __module_name__ = "HexChat random channel plugin"
 __module_version__ = "0.0.1"
 __module_description__ = "randchan cmd"
 __author__ = "Rylee Elise Fowler"
-import xchat as HC
 
-def randchan (length=10):
-    HC.command('join' + ''.join(random.sample(string.lowercase, 10))
+def randchan (word, word_eol, userdata):
+    xchat.command('join #' + ''.join(random.sample(string.lowercase, 10)))
 
-HC.hook_command("randchan", randchan)
-HC.prnt(__module_name__ + ' version ' + __module_version__ + ' loaded.')
+xchat.hook_command("randchan", randchan)
+xchat.prnt(__module_name__ + ' version ' + __module_version__ + ' loaded.')
