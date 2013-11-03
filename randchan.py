41d693f972425297031ab3a2359c1a998bcb8f39
Rylai <gando.depth@gmail.com>
2013-11-03 14:09:24 -0500

    randchan: s/HC/XC/ for hexchat style

--- randchan.py
+++ randchan.py
@@ -1,6 +1,6 @@
 import string
 import random
-import xchat as HC
+import xchat as XC
 
 __module_name__ = "HexChat random channel plugin"
 __module_version__ = "0.0.1"
@@ -8,7 +8,7 @@ __module_description__ = "randchan cmd"
 __author__ = "Rylee Elise Fowler"
 
 def randchan (length=10):
-    HC.command('join' + ''.join(random.sample(string.lowercase, 10))
+    XC.command('join' + ''.join(random.sample(string.lowercase, 10))
 
-HC.hook_command("randchan", randchan)
-HC.prnt(__module_name__ + ' version ' + __module_version__ + ' loaded.')
+XC.hook_command("randchan", randchan)
+XC.prnt(__module_name__ + ' version ' + __module_version__ + ' loaded.')
