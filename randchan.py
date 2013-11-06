569eca1438a42c7cf2e3cd7520bd94f8eec0e5ee
Rylai <gando.depth@gmail.com>
2013-11-06 02:53:30 -0500

    randchan: change string.lowercase to string.ascii_lowercase this is for python3 compatibility

--- randchan.py
+++ randchan.py
@@ -8,7 +8,7 @@ __module_description__ = "randchan cmd"
 __author__ = "Rylee Elise Fowler"
 
 def randchan (word, word_eol, userdata):
-    xchat.command('join #' + ''.join(random.sample(string.lowercase, 10)))
+    xchat.command('join #' + ''.join(random.sample(string.ascii_lowercase, 10)))
 
 xchat.hook_command("randchan", randchan)
 xchat.prnt(__module_name__ + ' version ' + __module_version__ + ' loaded.')
